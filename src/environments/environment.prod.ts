1e46f95e391365635fb3cd7af8a3e26dedfe9910
seakh-masum <masum0aktar@gmail.com>
2022-12-11 21:43:02 +0530

    tailwind added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
+  TAILWIND_MODE: 'build',
   firebaseConfig: {
-    apiKey: "AIzaSyA10_Hne0PlOMyjpl3AMXVZWPczLyBJHaE",
-    authDomain: "masum-cv.firebaseapp.com",
-    databaseURL: "https://masum-cv.firebaseio.com",
-    projectId: "masum-cv",
-    storageBucket: "masum-cv.appspot.com",
-    messagingSenderId: "733514146710",
-    appId: "1:733514146710:web:556a863030a39819b673b7",
-    measurementId: "G-1WQGXG27F3"
-  }
+    apiKey: 'AIzaSyA10_Hne0PlOMyjpl3AMXVZWPczLyBJHaE',
+    authDomain: 'masum-cv.firebaseapp.com',
+    databaseURL: 'https://masum-cv.firebaseio.com',
+    projectId: 'masum-cv',
+    storageBucket: 'masum-cv.appspot.com',
+    messagingSenderId: '733514146710',
+    appId: '1:733514146710:web:556a863030a39819b673b7',
+    measurementId: 'G-1WQGXG27F3',
+  },
 };
