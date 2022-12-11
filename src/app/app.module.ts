96cdf341c1e06c927ae1d19555ea1e147842953b
seakh-masum <masum0aktar@gmail.com>
2022-12-11 23:57:00 +0530

    remove httpclient as not required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { HttpClientModule } from '@angular/common/http';
 import { components } from './components';
 @NgModule({
   declarations: [AppComponent, ...components],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
